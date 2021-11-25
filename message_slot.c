faf706c7296a317a4fa5ebf16a3e22e1da0679e7
student <tomerbrown@mail.tau.ac.il>
2021-11-25 20:48:34 +0200

    For now everything excists and compiles

--- message_slot.c
+++ message_slot.c
@@ -1,23 +1,25 @@
+// todo: move all of these defines to the header file
+#include <linux/ioctl.h>
 #define MAJOR_NUMBER 240
 #define MESSAGE_MAX_LEN 128
 #define DEVICE_NAME "message_slot"
 #define SUCCESSFUL 0
+#define MSG_SLOT_CHANNEL _IOW(MAJOR_NUMBER, 0, unsigned long)
 
 
 #undef __KERNEL__
 #define __KERNEL__
-
-
 #undef MODULE
 #define MODULE
 
+
+// include everything neccessary for the module to run
 #include <linux/kernel.h>
 #include <linux/module.h>
 #include <linux/init.h>
 #include <linux/fs.h>
 #include <linux/uaccess.h>
 #include <linux/kern_levels.h>
-#include <linux/ioctl.h>
 #include <linux/string.h> 
 #include <linux/errno.h>
 #include  <linux/slab.h>
@@ -69,10 +71,12 @@ SlotList initList(void){
     return lst;
 }
 
-/*A function that given all parameters of the nodes append it to the end of the list*/
+/*A function that given all parameters of the nodes append it to the end of the list
+* If failed to allocate new memomry as needed returns -1
+*/
 int appendMessage (SlotList* lst, char* message , int messageLen , unsigned int minorNum){
     int i;
-    MessageNode* newMessageNode = (MessageNode*) kmalloc(sizeof(MessageNode),GFP_ATOMIC);
+    MessageNode* newMessageNode = (MessageNode*) kmalloc(sizeof(MessageNode),GFP_KERNEL);
     if (newMessageNode == NULL){
         /*todo: Check what to in this case*/
         return -1;
@@ -175,24 +179,112 @@ void printList(SlotList* lst){
  * **********************************************************************************************************************/
 
 
-/*****************************************   Variables Used Globally by the program*************************************/
+/*=======================================  Variables Used Globally by the Module ========================================*/
 
 static SlotList slots_lst;
 
+
+/*=========================================== Implemantaion of device functions ==============================================*/
+
 static int device_open( struct inode* inode, struct file*  file ) {
+    unsigned int minorNumber;
+    MessageNode* messageNode;
+
+    minorNumber = iminor(inode);
+    messageNode = findByMinorNumber(&slots_lst, minorNumber);
+
+    if (messageNode == NULL){
+        //It means that the needed node for minor number doesn't excists - then create a blank one
+        if (appendMessage(&slots_lst , "" , 0 , minorNumber)<0){
+            printk(KERN_ERR "Error in allocating memory for the new slot");
+            return -1;
+        }
+    }
     return SUCCESSFUL;
 }
 
-static ssize_t device_ioctl (struct file* fd , unsigned int control_command , unsigned long commandaParameter){
+static ssize_t device_ioctl (struct file* fd , unsigned int control_command , unsigned long commandParameter){
+    int minorNumber;
+    MessageNode* messageNode;
+
+    if (control_command != MSG_SLOT_CHANNEL){
+        printk(KERN_ERR "ioctl isn't MSG_SLOT_CHANNEL\n");
+        return -EINVAL;
+    }
+    if (commandParameter== 0){
+        printk(KERN_ERR "ioctl control channel is 0\n");
+        return -EINVAL;
+    }
+
+    minorNumber = *(int*)(fd->private_data);
+    messageNode = findByMinorNumber(&slots_lst , minorNumber);
+
+    if (messageNode == NULL){
+        printk(KERN_ERR "Slot wasn't opened yet \n");
+        return -EINVAL;
+    }
+
+    fd->private_data = (void*) (&commandParameter);
     return SUCCESSFUL;
 }
 
-static ssize_t device_read (struct file* fd, char* buffer , size_t buffer_size,  loff_t* offset){
-    return SUCCESSFUL;
+static ssize_t device_read (struct file* fd, char* buffer , size_t buffer_size, loff_t* offset){
+    int minorNumber;
+    int i;
+    MessageNode* node;
+
+    minorNumber =  *(int*)(fd->private_data);
+    node = findByMinorNumber(&slots_lst , minorNumber);
+
+    if (node==NULL){
+        //Such file as requested does not exsits
+        return -EINVAL;
+    }
+
+    if (node->len == 0){
+        return -EWOULDBLOCK;
+    }
+
+    if (buffer_size < node->len){
+        return -ENOSPC;
+    }
+
+    /*Copy to buffer char by char*/
+    for (i=0; i< node->len; i++){
+        if (put_user(node->message[i],buffer+i)<0){
+            printk(KERN_ERR "Error copying from kernel space to user space \n");
+            return -1;
+        }
+    }
+    //At the end i should be exactly how many charachters read.
+    return i;
 }
 
-static ssize_t device_write (struct file* fd, const char* buffer , size_t buffer_size,  loff_t* offset){
-    return SUCCESSFUL;
+static ssize_t device_write (struct file* fd, const char* buffer , size_t buffer_size, loff_t* offset){
+    int minorNumber;
+    int i;
+    MessageNode* node;
+
+    minorNumber = *(int*)(fd->private_data);
+    node = findByMinorNumber(&slots_lst , minorNumber);
+
+    if (buffer_size<= 0 || buffer_size >128){
+        return -EMSGSIZE;
+    }
+
+    if (node== NULL){
+        return -EINVAL;
+    }
+
+    for (i=0;i<buffer_size;i++){
+        if (get_user(node->message[i],&buffer[i])<0){
+            printk(KERN_ERR "Error in get_user function call inside device_write");
+            return -1;
+        }
+    }
+    node->len = buffer_size;
+
+    return i;
 }
 
 
@@ -225,6 +317,11 @@ static int messageSlot_init(void){
 }
 
 
+/*A function to run when the Module finishes (rmmod in command line) 
+The Function do the following:
+    1) Unregister the char device
+    2) Frees the slot list allocated when running
+*/
 static void messageSlot_cleanup(void){
   printk( "Cleaning up hello module.\n");
   free_list (&slots_lst);
@@ -232,5 +329,7 @@ static void messageSlot_cleanup(void){
 }
 
 
+
+/*********************************** Loading the init and exit function to Module *************************************/
 module_init(messageSlot_init);
 module_exit(messageSlot_cleanup);
